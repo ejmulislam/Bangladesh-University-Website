f8b3e57c71f55bb427f56f46225f4950cf155242
Your Name <you@example.com>
2023-01-06 20:58:38 +0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container mx-auto">
+    <header class="shadow-lg">
+      <ul class="p-12 py-6">
+        <li class="space-x-4">
+          <!--ROUTER LINKS-->
+          <RouterLink to="/" class="hover:text-red-500 transition duration-300"> Home </RouterLink>
+          <RouterLink to="/about" class="hover:text-red-500 transition duration-300"> About </RouterLink>
+        </li>
+      </ul>
+    </header>
+    <!--ROUTER VIEW-->
+    <RouterView></RouterView>
+  </div>
+</template>
---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="p-12 py-6">
+        Aboutpage
+    </div>
+</template>
